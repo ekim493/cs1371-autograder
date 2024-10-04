4edb6d5455be61d9ef18f20a20180a001cce07e9
ekim493 <ekim493@gatech.edu>
2024-10-04 13:29:55 -0400

    Local tester fix

--- Local_Tester.m
+++ Local_Tester.m
@@ -1,8 +1,9 @@
 assignment_name = 'HW0'; % Edit
 
 addpath("Submissions\")
+addpath(sprintf('source/solutions/%s', assignment_name))
 cd("source\")
-metadata = jsondecode(fileread('submission_metadata.json'));
+metadata = jsondecode(fileread('../submission_metadata.json'));
 metadata.assignment.title = assignment_name;
 json = jsonencode(metadata);
 fh = fopen('../Submissions/submission_metadata.json', 'w');
