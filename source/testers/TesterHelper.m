f8ac4c45846a4ea15b8081fda770acfd0a320d1e
ekim493 <ekim493@gatech.edu>
2024-10-16 11:03:43 -0400

    feat: Added generateCellArray

--- source/testers/TesterHelper.m
+++ source/testers/TesterHelper.m
@@ -17,6 +17,7 @@ classdef TesterHelper
             %   Arguments
             %       varargin - Input arguments as if directly calling a student's function.
             %       varargout - Outputs will be the same as the student's function.
+
             try
                 stack = dbstack;
                 funcFile = char(extractBetween(stack(2).name, '.', '_Test'));
@@ -853,6 +854,75 @@ classdef TesterHelper
             OPS = cellfun(@upper, iskeyword, 'UniformOutput', false);
             calls = [calls reshape(string(info.mtfind('Kind', OPS).kinds), 1, [])];
             calls = unique(calls);
+        end
+
+        function out = generateCellArray(options)
+
+            % GENERATECELLARRAY - Generates a simple cell array containing various data types.
+            %   This function generates a cell array containing a random data type, including doubles, vectors, logical
+            %   vectors, and strings. By default, it will generate all of them with a size of c = [3, 5] and r = 1.
+            %
+            %   Name-Value Arguments
+            %       rows (double) - Specify the number of rows the cell array should have. Specify a range of possible
+            %                       values by inputing a (1, 2) double vector. Default = 1.
+            %       columns (double) - Specify the number of rows the cell array should have. Specify a range of possible
+            %                          values by inputing a (1, 2) double vector. Default = [3, 5].
+            %       doubles (logical) - Specify whether the cell array should contain single doubles. These doubles will 
+            %                           be random integers in the range specified by doubleRange. Default = true.
+            %       vectors (logical) - Specify whether the cell array should contain vectors of doubles. These doubles
+            %                           will be random integers in the range specified by doubleRange, and the vector 
+            %                           will be 1 to 5 in length. Default = true.
+            %       strings (logical) - Specify whether the cell array should contain vectors of chars. These chars will
+            %                           be a random string of 5 to 10 lowercase letters. Default = true.
+            %       logicals (logicals) - Specify whether the cell array should contain vectors of logicals. The length
+            %                             of this vector will be 1 to 5. Default = true.
+            %       doubleRange (double) - Specify a range of values for all random double number generation. Default =
+            %                              [0, 100].
+            %       stringIsSent (logical) - Specify whether the strings should be generated in a sentence like format.
+            %                                This will include spaces and increase the length to 20 to 40. Default = false.
+            
+            arguments
+                options.rows (1, :) double = 1
+                options.columns (1, :) double = [3, 5]
+                options.vectors (1, 1) logical = true
+                options.doubles (1, 1) logical = true
+                options.strings (1, 1) logical = true
+                options.logicals (1, 1) logical = true
+                options.doubleRange (1, 2) double = [0, 100]
+                options.stringIsSent (1, 1) logical = false
+            end
+
+            if isscalar(options.rows)
+                r = options.rows;
+            else
+                r = randi(options.rows);
+            end
+            if isscalar(options.columns)
+                c = options.columns;
+            else
+                c = randi(options.columns);
+            end
+            ca = cell([r, c]);
+            pos = 'vdsl';
+            pos = pos([options.vectors, options.doubles, options.strings, options.logicals]);
+            for i = 1:numel(ca)
+                type = pos(randi(numel(pos)));
+                switch type
+                    case 'v'
+                        ca{i} = randi(options.doubleRange, [1, randi(5)]);
+                    case 'd'
+                        ca{i} = randi(options.doubleRange);
+                    case 's'
+                        if options.stringIsSent
+                            ca{i} = TesterHelper.generateString(sentence=true, length=[20, 40]);
+                        else
+                            ca{i} = TesterHelper.generateString(length=[5, 10]);
+                        end
+                    case 'l'
+                        ca{i} = logical(randi([0, 1], [1, randi(5)]));
+                end
+            end
+            out = ca;
         end
 
         function out = generateString(options)
