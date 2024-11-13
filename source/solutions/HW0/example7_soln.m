d896927e9877275571e8f18cea76a7a805499e91
ekim493 <ekim493@gatech.edu>
2024-11-13 14:36:11 -0500

    Fixed example7 and added imshow to allowed funcs

--- source/solutions/HW0/example7_soln.m
+++ source/solutions/HW0/example7_soln.m
@@ -2,5 +2,5 @@ function num = example7_soln(fn)
 img = imread(fn);
 num = sum(img(:, :, 1) > 100, 'all');
 img = img(end:-1:1, end:-1:1, :);
-imwrite(img, [fn(1:end-4) '_updated_soln.png']);
+imwrite(img, [fn(1:end-4) '_updated.png']);
 end
